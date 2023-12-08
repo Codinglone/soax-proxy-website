acad0543ada75cd38f163a66d8ab396ce010430e
Codinglone <codinglone@gmail.com>
2023-12-08 10:43:30 +0200

    Few UI modifications

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const ProductsSection = () => {
   return (
-    <div>ProductsSection</div>
-  )
-}
+    <div>
+      <h1 className="text-[42.6624px]">
+      Explore seamless data extraction with SOAX Proxies and APIs.
+      </h1>
+    </div>
+  );
+};
 
-export default ProductsSection
+export default ProductsSection;
